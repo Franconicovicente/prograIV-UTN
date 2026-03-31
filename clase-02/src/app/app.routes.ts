3d7b158bc979937b773dc55ff81c8c82dce3f321
Franconicovicente <franconicolassvicente@gmail.com>
2026-03-31 00:46:50 -0300

    Cambios en clase 02

--- clase-02/src/app/app.routes.ts
+++ clase-02/src/app/app.routes.ts
@@ -1,10 +1,6 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/auth/login/login';
 import { Auth } from './pages/auth/auth';
-
-// import { Bienvenida } from './pages/bienvenida/bienvenida';
-// import { Error } from './pages/error/error';
-// import { Login } from './pages/login/login';
 import { Registro } from './pages/auth/registro/registro';
 
 export const routes: Routes = [
@@ -28,37 +24,10 @@ export const routes: Routes = [
     */
    
     {
-<<<<<<< HEAD
-        path: 'auth', 
-        children: [
-            {
-                path: "login",
-                component: Login
-            },
-            
-            {
-                path: "registro",
-                component: Registro
-            },
-
-        ]
-=======
         path: 'auth',
         loadComponent: () => import("./pages/auth/auth").then((module) => module.Auth),
         loadChildren: () => import("./pages/auth/auth.routes").then((routes) => routes.authRoutes),
->>>>>>> 2dee6c8fecb959c3da085d9ba46cfed90f10b0c6
     },
-
-    // {
-    //     path: "login",
-    //     loadComponent: () => import("./pages/login/login").then((module) => module.Login)
-    // },
-
-    // {
-    //     path: "registro",
-    //     loadComponent: () => import("./pages/registro/registro").then((module) => module.Registro)
-    // },
-
 
      // WILDCARD
     {
